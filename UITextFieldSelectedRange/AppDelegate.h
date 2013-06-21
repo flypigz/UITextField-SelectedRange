d042fe518edc93913d0ad36fd123ba7b96e138e1
Justin Zeng <flypigz@gmail.com>
2013-06-22 01:33:33 +0800

    Initial Commit

---
+++ UITextFieldSelectedRange/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITextFieldSelectedRange
+//
+//  Created by Justin on 13-6-22.
+//  Copyright (c) 2013年 apple.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
